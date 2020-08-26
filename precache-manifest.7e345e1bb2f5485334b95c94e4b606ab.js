01abec1d75ba59026cbc8236131ed71348bb44e2
Anisha Desai <desaianisha95@gmail.com>
2020-08-26 14:51:06 -0700

    Updates

---
+++ precache-manifest.7e345e1bb2f5485334b95c94e4b606ab.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667a102fde505e6bc3c0bfb1f1b79462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6122713468aad068b2e",
+    "url": "/static/css/2.a29b8585.chunk.css"
+  },
+  {
+    "revision": "931529cfb374e633ddad",
+    "url": "/static/css/main.701a4650.chunk.css"
+  },
+  {
+    "revision": "b6122713468aad068b2e",
+    "url": "/static/js/2.cce1602a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cce1602a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931529cfb374e633ddad",
+    "url": "/static/js/main.e2a0fda6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5ae919248bc512131af1280e5e28697f",
+    "url": "/static/media/Anisha.5ae91924.webp"
+  },
+  {
+    "revision": "b3b34a83fa45ca2ee7da90daaf61f15f",
+    "url": "/static/media/Models.b3b34a83.webp"
+  },
+  {
+    "revision": "fa53414bc1003eb3aca74e3b195ffa27",
+    "url": "/static/media/Slide1.fa53414b.webp"
+  },
+  {
+    "revision": "887bdd561c23526f520eb2b2ecfda366",
+    "url": "/static/media/Slide2.887bdd56.webp"
+  },
+  {
+    "revision": "018ccad6bf2a58e5813b5a4d57154174",
+    "url": "/static/media/Slide3.018ccad6.webp"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "52d036900f282f4cb4ae30bc73bd9481",
+    "url": "/static/media/atom.52d03690.svg"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "30d8879f6268e2a2bcdd011e6ab24996",
+    "url": "/static/media/bootstrap.30d8879f.svg"
+  },
+  {
+    "revision": "889449e31190719bac49e10699663581",
+    "url": "/static/media/css3.889449e3.svg"
+  },
+  {
+    "revision": "594910eb387b083581fbc3292fc99095",
+    "url": "/static/media/eclipse.594910eb.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "add0a4e64cc725be9b1150a00203a673",
+    "url": "/static/media/git.add0a4e6.svg"
+  },
+  {
+    "revision": "7697c1bf2fc67bdd49d7f50c4b34d1a1",
+    "url": "/static/media/github-pages.7697c1bf.svg"
+  },
+  {
+    "revision": "5e025b389b303c0d010a3294eb183d21",
+    "url": "/static/media/html.5e025b38.svg"
+  },
+  {
+    "revision": "cce27e614f7e248cfc9827467f46c01f",
+    "url": "/static/media/java.cce27e61.svg"
+  },
+  {
+    "revision": "5b54675950638dd95de282256b5cf7c4",
+    "url": "/static/media/javascript.5b546759.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dd62bad8d530a7b1f5e87b669f74fd80",
+    "url": "/static/media/mySql.dd62bad8.svg"
+  },
+  {
+    "revision": "3345f360226545fd7b7ee0b9c3dc52d5",
+    "url": "/static/media/mysqlWorkbench.3345f360.svg"
+  },
+  {
+    "revision": "01766e9cdd6e12e020a2c9ca505fd961",
+    "url": "/static/media/python.01766e9c.svg"
+  },
+  {
+    "revision": "60957d8927c61b23bec310ee89f17176",
+    "url": "/static/media/react-bootstrap.60957d89.svg"
+  },
+  {
+    "revision": "1f6a76a4bf66ee1d6bdeab1edc5e2b5c",
+    "url": "/static/media/react.1f6a76a4.svg"
+  },
+  {
+    "revision": "3e3945f404f289a8107547791307bb5c",
+    "url": "/static/media/redux.3e3945f4.svg"
+  },
+  {
+    "revision": "b445e745baa125347763e169f249001b",
+    "url": "/static/media/robo.b445e745.webp"
+  },
+  {
+    "revision": "03bf46d11ea9a09f330c343a9d8eb645",
+    "url": "/static/media/selenium.03bf46d1.svg"
+  },
+  {
+    "revision": "28c50f1a7d28b935dd38acf44360f0e3",
+    "url": "/static/media/spring.28c50f1a.svg"
+  },
+  {
+    "revision": "c359edc3eab1044ce7db23cdc18f6743",
+    "url": "/static/media/sublime.c359edc3.svg"
+  },
+  {
+    "revision": "2e11405e584527d67a5312f270394849",
+    "url": "/static/media/visual-studio-code.2e11405e.svg"
+  },
+  {
+    "revision": "4a85c0ed1300a4ca71867b5174035316",
+    "url": "/static/media/weather.4a85c0ed.webp"
+  }
+]);
